c985163c10f153ca8904c350d25482baa6969310
Minyoui <evanlyrush73@gmail.com>
2024-01-05 14:09:47 +0800

    first commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:diceko/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:diceko/screens/location_screen.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getWeatherData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+          child: SpinKitWave(
+        size: 60.0,
+        color: Colors.white,
+      )),
+    );
+  }
+
+  void getWeatherData() async {
+    Networking networking = new Networking();
+    String data = await networking.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(data);
+    }));
+  }
+}
